604737271e5ebd05611c6a3f1489f269e82a04fc
SangHyun Kim <ksh9891@gmail.com>
2024-05-05 14:42:26 +0900

    props 를 통해 데이터 전달하기2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-function Square({ value }) {
+function Square({ value }: { value: number }) {
   return <button className="square">{value}</button>;
 }
 
@@ -6,19 +6,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
       </div>
     </>
   );
