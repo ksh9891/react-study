7317768845226d6bdceafd8841eb0d3cf5478563
SangHyun Kim <ksh9891@gmail.com>
2024-05-18 15:47:15 +0900

    시간여행 구현하기

--- src/App.tsx
+++ src/App.tsx
@@ -67,14 +67,20 @@ function Board({
 export default function Game() {
   const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [history, setHistory] = useState<Array<Array<ValueType>>>([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState<number>(0);
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: Array<ValueType>) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
-  function jumpTo(nextMove) {}
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
 
   const moves = history.map((squares, move) => {
     let description;
@@ -85,7 +91,7 @@ export default function Game() {
     }
 
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
