241d7985ecd8e087151acc977e63db60ceeea646
SangHyun Kim <ksh9891@gmail.com>
2024-05-18 15:49:21 +0900

    최종 정리

--- src/App.tsx
+++ src/App.tsx
@@ -65,24 +65,22 @@ function Board({
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [history, setHistory] = useState<Array<Array<ValueType>>>([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState<number>(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: Array<ValueType>) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
-  const moves = history.map((squares, move) => {
+  const moves = history.map((_squares, move) => {
     let description;
     if (move > 0) {
       description = 'Go to move #' + move;
